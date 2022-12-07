ac6eedfcc514a49037abac00a8fb4d30940c7af5
Meradikonda <90790969+Meradikonda@users.noreply.github.com>
2022-12-06 22:38:15 -0500

    Delete Method.java

--- Method.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.*;
-
-public interface Method extends Remote{
-
-	public String action()throws RemoteException;
-
-	//callback implementation
-	public String getClient(String Input) throws  RemoteException;
-}
